645966dd53acb0033a71d2def1ad2a6bf67d1fe2
John Osinski <johnosinski80@gmail.com>
2023-01-20 17:11:12 -0500

    Updated workspaces modules to use colors and icons

--- config/lemonbar/kclemonbar.sh
+++ config/lemonbar/kclemonbar.sh
@@ -6,6 +6,39 @@
 # Heavy inspiration from JLErvin's simplebar script (github)
 # as well as simonhughxyz's lemonbar script (github)
 
+#############################
+# CONFIGS / SETUP           #
+#############################
+
+# lemonbar options
+set_bar_height=28
+set_bar_font="Inconsolata Nerd Font" # Font Awesome 5 Free Solid is also used
+set_bar_font_size=10
+set_bar_underline=3
+
+#colors setup, read from .Xresources
+cFG=$( xrdb -query | grep "*.foreground" | cut -f 2 )
+cBG=$( xrdb -query | grep "*.background" | cut -f 2 )
+cBLACK=$( xrdb -query | grep "*.color0" | cut -f 2 )
+cBLACK_L=$( xrdb -query | grep "*.color8" | cut -f 2 )
+cRED=$( xrdb -query | grep "*.color1" | cut -f 2 )
+cRED_L=$( xrdb -query | grep "*.color9" | cut -f 2 )
+cGREEN=$( xrdb -query | grep "*.color2" | cut -f 2 )
+cGREEN_L=$( xrdb -query | grep "*.color10" | cut -f 2 )
+cYELLOW=$( xrdb -query | grep "*.color3" | cut -f 2 )
+cYELLOW_L=$( xrdb -query | grep "*.color11" | cut -f 2 )
+cBLUE=$( xrdb -query | grep "*.color4" | cut -f 2 )
+cBLUE_L=$( xrdb -query | grep "*.color12" | cut -f 2 )
+cMAGENTA=$( xrdb -query | grep "*.color5" | cut -f 2 )
+cMAGENTA_L=$( xrdb -query | grep "*.color13" | cut -f 2 )
+cCYAN=$( xrdb -query | grep "*.color6" | cut -f 2 )
+cCYAN_L=$( xrdb -query | grep "*.color14" | cut -f 2 )
+cWHITE=$( xrdb -query | grep "*.color7" | cut -f 2 )
+cWHITE_L=$( xrdb -query | grep "*.color15" | cut -f 2 )
+
+
+
+
 # create named pipe
 prefix_dir="$HOME/.config/lemonbar"
 panel_fifo="$prefix_dir/panel-fifo"
@@ -19,9 +52,81 @@ mkfifo "${panel_fifo}"
 
 workspaces() {
 	bspc subscribe report | while read -r line; do
-		desktops=$( echo ${line} | tr ':' '\n' | sed '/[WM|L|T|G]/d;')	
-		desktops_formatted="$( echo ${desktops} | tr '\n' ' ' )"
-		echo "WORKSPACES ${desktops_formatted}"
+		# parse subscribe report into an array of desktop elements
+        desktops=$( echo ${line} | tr ':' '\n' | sed '/[WM|L|T|G]/d;')	
+        desktops_formatted=($( echo ${desktops} | tr '\n' ' ' ))
+
+        # wspace will hold the final workspaces string for the bar
+        # underline tracks if a given elemement will be underlined
+        #   because it has active notes on on that desktop
+        # focused tracks if a given element will be highlighted
+        #   because that desktop is the current focus
+        #
+        # im going to use these variables by iterating through each
+        # desktop element in the array and see what it means / how 
+        # it should be formatted. It assumes 5 desktops named 
+        # I II III IV and V. See man bspc for info on the subscribe
+        # report that I am parsing.
+        wspace=""
+        underline=false
+        focused=false
+        for element in "${desktops_formatted[@]}"
+        do
+            # check if this is the currently focused desktop
+            if [[ "${element:0:1}" == "F" || "${element:0:1}" == "O" ]]; then
+                focused=true
+                # update the background for this item
+                wspace="${wspace}%{B${cBLACK}}"
+            fi
+
+            # checking if this is a desktop that has active notes
+            if [[ "${element:0:1}" == "o" || "${element:0:1}" == "O" ]]; then
+                if [[ $focused == true ]]; then
+                    wspace="${wspace}%{U${cCYAN}}%{+u}"
+                else
+                    wspace="${wspace}%{U${cMAGENTA}}%{+u}"
+                fi
+                underline=true
+            fi
+
+            # check to see what character I should print for each desktop
+            case ${element:1} in
+                I)
+                    wspace="${wspace}    "
+                    ;;
+                II)
+                    wspace="${wspace}    "
+                    ;;
+                III)
+                    wspace="${wspace}    "
+                    ;;
+                IV)
+                    wspace="${wspace}    "
+                    ;;
+                V)
+                    wspace="${wspace}    "
+                    ;;
+                *) ;;
+            esac
+
+            # clean up if the element was underlined
+            if [[ $underline == true ]]; then
+                wspace="${wspace}%{-u}"
+                underline=false
+            fi
+
+            # clean up if the element was focused
+            if [[ $focused == true ]]; then
+                wspace="${wspace}%{B${cBG}}"
+                focused=false
+            fi
+
+            # small space between each desktop
+            wspace="${wspace} "
+
+        done
+
+		echo "WORKSPACES ${wspace}"
 	done
 }
 
@@ -83,9 +188,9 @@ while read -r line; do
 			;;
 	esac
 
-	# build the bar
-	echo "%{l}%{B#000}%{F#FFF}${fn_workspaces}%{c}${fn_tspace}%{r}${fn_battery} ${fn_datetime}%{O${fn_tspace}}"
+    # build the bar (-e flag to allow echo with escape characters)
+	echo -e "%{l}${fn_workspaces}%{c}${fn_tspace}${cMAGENTA}%{r}${fn_battery} ${fn_datetime}%{O${fn_tspace}}"
 
 # at the end of the day, make sure the named pipe is providing updates
 # and the updates are piped into lemonbar
-done < "${panel_fifo}" | lemonbar | $SHELL
+done < "${panel_fifo}" | lemonbar -g x${set_bar_height} -u ${set_bar_underline} -f "Font Awesome 5 Free Solid-${set_bar_font_size}" -f "${set_bar_font}-${set_bar_font_size}" -B ${cBG} -F ${cFG} | $SHELL
