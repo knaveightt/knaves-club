43946c9dfdc8cc8773d8507883fdce7366fd54ee
John Osinski <johnosinski80@gmail.com>
2023-01-25 12:14:37 -0500

    Tweaks to colorscheming

--- config/trayer/start.sh
+++ config/trayer/start.sh
@@ -2,7 +2,7 @@
 
 # wait for 2 seconds before starting the system tray
 sleep 2
-trayer --edge top --align right --SetDockType true --transparent true --alpha 255 --SetPartialStrut true --expand true --height 28 --widthtype request --tint 0x000 &
+trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --height 28 --widthtype request --transparent true --alpha 0 --tint 0x211f1d &
 
 # start system tray applications, gradually after each second
 sleep 1
